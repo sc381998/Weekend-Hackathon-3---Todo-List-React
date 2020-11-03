434c78f6784b622a090225a650265c40ef3f32f6
Bishal Choudhary <bishalchoudhary67@gmail.com>
2020-11-03 11:34:46 +0530

    changes done

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -14,72 +14,66 @@ function ToDoItem(props) {
         className="list"
         style={{ display: "flex", justifyContent: "space-between" }}
       >
-        {props.onShow ? (
-          <div>
-            <input
-              type="text"
-              className="editTask"
-              defaultValue={props.text}
-              onChange={props.onEditChange}
-            />
+        <li style={{ textDecoration: isDone ? "line-through" : "none" }}>
+          {props.text.substring(0, 30)}
+        </li>
+        <div>
+          <div style={{ display: "flex", lineHeight: "2.3" }}>
             <span
-              className="saveTask pointer"
-              onClick={() => props.onSave(props.id)}
-              disabled={!props.checkSaveButton}
+              className="pointer edit"
+              onClick={() => props.onEdit(props.id)}
               role="img"
               aria-label="done"
             >
-              ✅
+              <i className="fa fa-pencil editIcon" aria-hidden="true"></i>
             </span>
             <span
-              className="cancelTask pointer"
-              onClick={() => props.onCancel(props.id)}
-              disabled={!props.checkSaveButton}
+              className="pointer"
+              onClick={handleClick}
               role="img"
               aria-label="done"
             >
-              ❌
+              <i className="fa fa-thumb-tack pinIcon" aria-hidden="true"></i>
+            </span>
+            <span
+              className="pointer delete"
+              onClick={() => props.onDelete(props.id)}
+              role="img"
+              aria-label="delete"
+            >
+              <i className="fa fa-trash deleteIcon" aria-hidden="true"></i>
             </span>
           </div>
-        ) : (
-          <>
-            <li style={{ textDecoration: isDone ? "line-through" : "none" }}>
-              {props.text.substring(0, 30)}
-            </li>
-            <div>
-              <div style={{ display: "flex", lineHeight: "2.3" }}>
-                <span
-                  className="pointer edit"
-                  onClick={() => props.onEdit(props.id)}
-                  role="img"
-                  aria-label="done"
-                >
-                  <i className="fa fa-pencil editIcon" aria-hidden="true"></i>
-                </span>
-                <span
-                  className="pointer"
-                  onClick={handleClick}
-                  role="img"
-                  aria-label="done"
-                >
-                  <i
-                    className="fa fa-thumb-tack pinIcon"
-                    aria-hidden="true"
-                  ></i>
-                </span>
-                <span
-                  className="pointer delete"
-                  onClick={() => props.onDelete(props.id)}
-                  role="img"
-                  aria-label="delete"
-                >
-                  <i className="fa fa-trash deleteIcon" aria-hidden="true"></i>
-                </span>
-              </div>
-            </div>
-          </>
-        )}
+        </div>
       </div>
+      {props.onShow && (
+        <div>
+          <input
+            type="text"
+            className="editTask"
+            defaultValue={props.text}
+            onChange={props.onEditChange}
+          />
+          <span
+            className="saveTask pointer"
+            onClick={() => props.onSave(props.id)}
+            disabled={!props.checkSaveButton}
+            role="img"
+            aria-label="done"
+          >
+            ✅
+          </span>
+          <span
+            className="cancelTask pointer"
+            onClick={() => props.onCancel(props.id)}
+            disabled={!props.checkSaveButton}
+            role="img"
+            aria-label="done"
+          >
+            ❌
+          </span>
+        </div>
+      )}
     </>
   );
 }
